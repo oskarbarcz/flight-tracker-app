094c89da4e7469d8c4aac3ece5c41806ae4a6a28
Oskar Barcz <ob@gi.org.pl>
2025-01-26 03:26:53 +0100

    Bump packages for release

--- app/components/Layout/Footer.tsx
+++ app/components/Layout/Footer.tsx
@@ -1,14 +1,19 @@
-import {FaHeart} from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
 
 export default function Footer() {
-
   return (
     <>
-      <div className="absolute right-5 bottom-5 text-sm text-gray-500">
+      <div className="absolute bottom-5 right-5 text-sm text-gray-500">
         {"Created with "}
         <FaHeart className="inline-block" />
         {" by "}
-        <a href="https://barcz.me" title="Let's get in touch!" className="font-bold text-teal-500">Oskar Barcz</a>
+        <a
+          href="https://barcz.me"
+          title="Let's get in touch!"
+          className="font-bold text-teal-500"
+        >
+          Oskar Barcz
+        </a>
       </div>
     </>
   );
