da3b457e2b93eb0c14f961c9dce3975617189ef0
Oskar Barcz <ob@gi.org.pl>
2025-02-02 22:45:37 +0100

    Unify form layouts

--- app/components/Box/PilotInformationBox.tsx
+++ app/components/Box/PilotInformationBox.tsx
@@ -15,7 +15,7 @@ export default function PilotInformationBox() {
   return (
     <section className="flex items-center rounded-lg bg-gray-100 p-4 shadow dark:bg-gray-800">
       <div className="mr-4 inline-flex size-10 items-center justify-center rounded-full bg-gray-100 dark:bg-gray-600">
-        <span className="font-medium text-gray-600 dark:text-gray-300">
+        <span className="text-gray-600 dark:text-gray-300">
           {getInitials(user.name)}
         </span>
       </div>
