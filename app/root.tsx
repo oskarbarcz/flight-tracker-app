5e166db1850907a38d3720a52c9db76e6ddb4bcf
Oskar Barcz <oskar@barcz.me>
2025-07-09 23:04:37 +0200

    Fix mobile navigation not diplaying cabin crew nav elements (#49)

--- app/root.tsx
+++ app/root.tsx
@@ -43,7 +43,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <ThemeModeScript />
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1 viewport-fit=cover"
+        />
         <meta name="apple-mobile-web-app-title" content="FlightTracker" />
         <Meta />
         <Links />
