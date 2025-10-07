1be4850ae3a2da689685d6427024496226cc52f0
Oskar Barcz <ob@gi.org.pl>
2025-10-07 20:05:33 +0200

    Add not found page article

--- app/state/api/adsb.service.ts
+++ app/state/api/adsb.service.ts
@@ -14,33 +14,6 @@ export class AdsbService extends AbstractApiService {
       `/api/v1/position/${trimmedCallsign}`,
     );
 
-    return [
-      {
-        callsign: "AAL4913",
-        date: "2025-01-01T13:10:00.000Z",
-        latitude: 60.317255208578054,
-        longitude: 24.958945837172326,
-      },
-      {
-        callsign: "AAL4913",
-        date: "2025-01-01T13:40:00.000Z",
-        latitude: 58.68825909518984,
-        longitude: 22.248460174236897,
-      },
-      {
-        callsign: "AAL4913",
-        date: "2025-01-01T14:10:00.000Z",
-        latitude: 55.587475442504896,
-        longitude: 17.204921862764106,
-      },
-      {
-        callsign: "AAL4913",
-        date: "2025-01-01T14:40:00.000Z",
-        latitude: 54.37998045994538,
-        longitude: 18.46850453127673,
-      },
-    ];
-
     return path.filter(
       (entry) => !(entry.latitude === 0 && entry.longitude === 0),
     );
