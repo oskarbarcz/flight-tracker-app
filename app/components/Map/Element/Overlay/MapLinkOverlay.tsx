10acdc4f94d0b6ff7a1d0f599acae3a4ab0bf37e
Oskar Barcz <ob@gi.org.pl>
2025-10-13 23:19:12 +0200

    Update map linking

---
+++ app/components/Map/Element/Overlay/MapLinkOverlay.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Button } from "flowbite-react";
+import { Link } from "react-router";
+import { useTrackedFlight } from "~/state/contexts/tracked-flight.context";
+import { IoIosLink } from "react-icons/io";
+import { LuExternalLink } from "react-icons/lu";
+
+export default function MapLinkOverlay() {
+  const { flight } = useTrackedFlight();
+
+  if (!flight) {
+    return null;
+  }
+
+  const handleCopy = () => {
+    const baseUrl = window.location.origin;
+    const trackingUrl = `${baseUrl}/live-tracking/${flight.id}`;
+    navigator.clipboard.writeText(trackingUrl).then();
+  };
+
+  return (
+    <div className="absolute top-3 right-3 flex gap-3">
+      <Button
+        as={Link}
+        to={`/live-tracking/${flight.id}`}
+        title="Open full-screen tracking in new tab"
+        target="_blank"
+        size="xs"
+        color="light"
+      >
+        <LuExternalLink />
+      </Button>
+      <Button
+        size="xs"
+        title="Copy tracking link to clipboard"
+        color="light"
+        onClick={handleCopy}
+      >
+        <IoIosLink />
+      </Button>
+    </div>
+  );
+}
