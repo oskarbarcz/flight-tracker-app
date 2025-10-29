19e83926a791b0fa38df850c7b376c6e87f2acce
Oskar Barcz <ob@gi.org.pl>
2025-10-29 23:29:44 +0100

    Implement elegant splash screen

--- app/components/Map/FullScreen/BottomBar.tsx
+++ app/components/Map/FullScreen/BottomBar.tsx
@@ -1,19 +1,15 @@
 import { FormattedLocalTime } from "~/components/Intrinsic/Date/FormattedLocalTime";
-import { useEffect, useState } from "react";
 
-export default function BottomBar() {
-  const [refreshedAt, setRefreshedAt] = useState(new Date());
+type Props = {
+  lastRefreshedAt: Date;
+};
 
-  useEffect(() => {
-    const id = setInterval(() => setRefreshedAt(new Date()), 5000);
-    return () => clearInterval(id);
-  }, []);
-
+export default function BottomBar({ lastRefreshedAt }: Props) {
   return (
     <div className="flex items-center justify-end mt-2 py-1 px-3 w-full">
       <span className="bg-gray-100 dark:bg-gray-900 text-sm text-gray-500 dark:text-gray-500 py-2 px-4 rounded-lg">
         {"Last update: "}
-        <FormattedLocalTime date={refreshedAt} seconds={true} />
+        <FormattedLocalTime date={lastRefreshedAt} seconds={true} />
       </span>
     </div>
   );
--- app/routes/public/PublicTrackingRoute.tsx
+++ app/routes/public/PublicTrackingRoute.tsx
@@ -1,21 +1,72 @@
 import { Route } from "../../../.react-router/types/app/routes/public/+types/PublicTrackingRoute";
-import { useLoaderData } from "react-router";
 import FullScreenMap from "~/components/Map/FullScreen/FullScreenMap";
 import TopBar from "~/components/Map/FullScreen/TopBar";
 import BottomBar from "~/components/Map/FullScreen/BottomBar";
+import { usePageTitle } from "~/state/hooks/usePageTitle";
+import { useAdsbData } from "~/state/contexts/adsb.context";
+import { usePublicApi } from "~/state/contexts/public-api.context";
+import { useEffect, useState } from "react";
+import { Flight } from "~/models";
+import MapSplash from "~/layout/MapSplash";
 
-export async function clientLoader({ params }: Route.ClientLoaderArgs) {
-  return { flightId: params.id };
-}
+export default function PublicTrackingRoute({
+  params,
+}: Route.ClientLoaderArgs) {
+  const { publicFlightService } = usePublicApi();
+  const { setCallsign, lastRequestedAt, flightPath, loadFlightPath } =
+    useAdsbData();
 
-export default function PublicTrackingRoute() {
-  const { flightId } = useLoaderData<typeof clientLoader>();
+  const [flight, setFlight] = useState<Flight | null>(null);
+  const [showSplash, setShowSplash] = useState<boolean>(true);
+
+  // fetch flight on mount
+  useEffect(() => {
+    const fetchFlight = async () => {
+      try {
+        const flightData = await publicFlightService.getById(params.id);
+        setFlight(flightData);
+        if (flightData) {
+          setCallsign(flightData.callsign);
+        }
+      } catch (error) {
+        console.error("Error fetching flight:", error);
+      }
+    };
+
+    fetchFlight();
+  }, [params.id, publicFlightService, setCallsign]);
+
+  // refresh
+  useEffect(() => {
+    if (!flight) return;
+
+    const intervalId = setInterval(() => {
+      loadFlightPath();
+    }, 10000); // 10 seconds
+
+    return () => clearInterval(intervalId);
+  }, [flight, loadFlightPath]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setShowSplash(false), 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  usePageTitle(
+    flight
+      ? `Tracking flight ${flight.flightNumber}`
+      : "Loading flight tracking...",
+  );
+
+  if (!flight || !lastRequestedAt || showSplash) {
+    return <MapSplash />;
+  }
 
   return (
     <div className="h-screen-safe flex flex-col items-stretch size-full p-2">
       <TopBar />
-      <FullScreenMap flightId={flightId} />
-      <BottomBar />
+      <FullScreenMap flight={flight} path={flightPath} />
+      <BottomBar lastRefreshedAt={lastRequestedAt} />
     </div>
   );
 }
