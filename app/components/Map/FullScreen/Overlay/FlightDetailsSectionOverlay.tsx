749fc73b1374f575a53881f7278a86753337410c
Oskar Barcz <ob@gi.org.pl>
2025-10-29 23:54:35 +0100

    Fix map on mobile devices

--- app/components/Map/FullScreen/Overlay/FlightDetailsSectionOverlay.tsx
+++ app/components/Map/FullScreen/Overlay/FlightDetailsSectionOverlay.tsx
@@ -1,13 +1,11 @@
 import BasicFlightInfoOverlay from "~/components/Map/FullScreen/Overlay/BasicFlightInfoOverlay";
 import { Flight } from "~/models";
 
-type FlightDetailsSectionOverlayProps = {
+type Props = {
   flight: Flight;
 };
 
-export default function FlightDetailsSectionOverlay({
-  flight,
-}: FlightDetailsSectionOverlayProps) {
+export default function FlightDetailsSectionOverlay({ flight }: Props) {
   return (
     <div className="absolute w-full flex flex-col gap-2 top-0 left-0 p-2 md:p-4 z-30">
       <BasicFlightInfoOverlay flight={flight} />
