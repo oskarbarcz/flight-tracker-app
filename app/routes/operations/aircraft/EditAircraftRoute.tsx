cdf91d188f063cebcb1f89f004afdf53f780c23e
Oskar Barcz <oskar@barcz.me>
2025-12-28 16:34:32 +0100

    Allow changing operator for aircraft

--- app/routes/operations/aircraft/EditAircraftRoute.tsx
+++ app/routes/operations/aircraft/EditAircraftRoute.tsx
@@ -29,7 +29,7 @@ export async function clientAction({
     "selcal",
     "registration",
     "livery",
-    "operatorId"
+    "operatorId",
   ]);
 
   await aircraftService.update(params.id, aircraft);
