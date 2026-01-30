a5a12b80d06e2ae3dc7db2fb0bdcd469c98b1fbf
Oskar Barcz <oskar@barcz.me>
2026-01-30 14:59:09 +0100

    Adjust border color in stats display

--- app/components/shared/Display/SimpleStatDisplay.tsx
+++ app/components/shared/Display/SimpleStatDisplay.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export default function SimpleStatDisplay({ icon, title, value }: Props) {
   return (
-    <article className="flex items-center gap-4 border border-gray-300 dark:border-gray-800 bg-white dark:bg-gray-900 py-3 px-4 rounded-xl">
+    <article className="flex items-center gap-4 border border-gray-200 dark:border-gray-800 bg-white dark:bg-gray-900 py-3 px-4 rounded-xl">
       {icon}
       <div className="font-bold">
         <h3 className="text-gray-500 uppercase text-xs">{title}</h3>
