4bd8989cadf41b83aabc381ba97d9a188a3760db
Oskar Barcz <oskar@barcz.me>
2026-02-03 01:55:25 +0100

    Implement flight OFP display and runway analysis

--- app/state/api/model/flight.dto.ts
+++ app/state/api/model/flight.dto.ts
@@ -8,7 +8,13 @@ import {
 
 export type CreateFlightRequest = Omit<
   ApiFlightResponse,
-  "id" | "airports" | "aircraft" | "operator" | "status" | "createdAt"
+  | "id"
+  | "airports"
+  | "aircraft"
+  | "operator"
+  | "status"
+  | "source"
+  | "createdAt"
 >;
 
 export type ApiLoadsheetResponse = {
