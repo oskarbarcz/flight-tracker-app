1fdd2d5368840d63a36f10dc0282de488e174dc0
Oskar Barcz <oskar@barcz.me>
2026-02-21 14:58:05 +0100

    Fix OFP layout and runway analysis layout on dark theme

--- app/components/flight/Dashboard/Tabs/Tab/FlightOfpTab.tsx
+++ app/components/flight/Dashboard/Tabs/Tab/FlightOfpTab.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Container from "~/components/shared/Layout/Container";
-import ContainerTitle from "~/components/shared/Layout/ContainerTitle";
 import { useTrackedFlight } from "~/state/contexts/global/tracked-flight.context";
 import useFlightOfp from "~/state/hooks/resources/useFlightOfp";
 
-export function unescapeOFP(html: string): string {
+function unescapeOFP(html: string): string {
   return html
     .replace(/\\n/g, "<br />") // Newlines
-    .replace(/\\"/g, '"') // Escaped quotes → real quotes
-    .replace(/\\'/g, "'") // Escaped apostrophes
     .replace(/\\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;") // Tabs
     .replace(/\\r/g, ""); // Remove carriage returns
 }
@@ -24,7 +21,7 @@ export default function FlightOfpTab() {
   return (
     <Container>
       <div
-        className="leading-tight overflow-auto max-h-186 text-black font-mono"
+        className="overflow-auto max-h-186 text-gray-700 dark:text-gray-300 font-mono"
         dangerouslySetInnerHTML={{ __html: unescapeOFP(ofp.ofpContent) || "" }}
       />
     </Container>
