6677b5b0e1c7e90f270bdfbaa1d9810a5018160b
Oskar Barcz <oskar.barcz@contractor.atolls.com>
2026-04-26 23:20:54 +0200

    Implement terminal management

--- app/components/shared/Table/LoadingStates/EmptyStateText.tsx
+++ app/components/shared/Table/LoadingStates/EmptyStateText.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export function EmptyStateText({ title, paragraph }: Props) {
   return (
-    <div className="md:w-3/4 lg:w-1/2 space-y-1.5 mx-auto mb-6 text-center dark:text-gray-200">
+    <div className="md:w-3/4 lg:w-1/2 space-y-1.5 mx-auto mb-6 text-center text-gray-900 dark:text-gray-200">
       <h2 className="font-bold text-lg">{title}</h2>
       {paragraph && <p className="text-gray-500">{paragraph}</p>}
     </div>
